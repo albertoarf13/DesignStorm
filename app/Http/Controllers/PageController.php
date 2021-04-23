f0c69f312de4d067fda2bade0ff5304777549895
Alberto <rivaalberto@gmail.com>
2021-04-23 20:07:40 +0200

    connect to API

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
+use GuzzleHttp\Client;
 
 class PageController extends Controller
 {
@@ -15,7 +15,17 @@ class PageController extends Controller
     }
 
     public function results(){
+
+        $client = new Client();
+
+        $res = $client->request('GET', 'https://api.unsplash.com/photos/?client_id=km-H2RmtWiVYODHf2KdHO5a7b-Zohpui70Mah0EI2uo');
+
+        $posts = $res->getBody();
+        $posts = json_decode($posts);
+
+        //return $posts;
+
         $user = Auth::user();
-        return view('pages/results', compact('user'));
+        return view('pages/results', compact('user', 'posts'));
     }
 }
--- resources/views/pages/results.blade.php
+++ resources/views/pages/results.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div id="site-section">
-  <div class="container">
+  <div class="container_own">
     <div id="results">
       <div>
         <div class="search-container">
@@ -12,6 +12,8 @@
         </div>
         <div class="boxes">
           <div class="row">
+            
+          <!--
             <div class="col-md-3">
               <div class="box">
                 <div style="position: relative; background: url('https://mir-s3-cdn-cf.behance.net/projects/202/4c23c557876077.Y3JvcCwxMjIyLDk1Niw4Nyww.jpg') no-repeat center center;-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover; height: 200px;">
@@ -40,6 +42,22 @@
                 </div>
               </div>
             </div>
+          -->
+
+            
+
+            @foreach($posts as $post)
+
+              <div class="col-md-3">
+                <div class="box">
+                  <div style="position: relative; background: url('{{$post->urls->regular}}') no-repeat center center;-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover; height: 200px;">
+                    <div class="add-btn "><i class="fa fa-check" aria-hidden="true"></i></div>
+                  </div>
+                </div>
+              </div>
+
+            @endforeach
+
           </div>
         </div>
       </div>
